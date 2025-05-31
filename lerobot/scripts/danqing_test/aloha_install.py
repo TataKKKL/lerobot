91f6e9dfd2320a60b51cf07a375e92a9c54d7fd6
Tata0703 <danqing.zhang.personal@gmail.com>
2025-05-31 15:37:23 -0700

    add pushT/ alpha scripts

--- lerobot/scripts/danqing_test/aloha_install.py
+++ lerobot/scripts/danqing_test/aloha_install.py
@@ -1,17 +1,21 @@
 # example.py
+import imageio
 import gymnasium as gym
-import gym_pusht
+import numpy as np
+import gym_aloha
 
-env = gym.make("gym_pusht/PushT-v0", render_mode="human")
+env = gym.make("gym_aloha/AlohaInsertion-v0")
 observation, info = env.reset()
+frames = []
 
 for _ in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
     image = env.render()
+    frames.append(image)
     print(observation)
-
     if terminated or truncated:
         observation, info = env.reset()
 
 env.close()
+imageio.mimsave("example.mp4", np.stack(frames), fps=25)
